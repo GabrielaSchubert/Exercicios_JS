5ba70764165304fe1b70c7b9ba8f27fcb3527d10
Gabriela Schubert <gabrielaschubert172@gmail.com>
2024-03-14 16:06:55 -0300

    exercicio script

--- my_script.js
+++ my_script.js
@@ -1,17 +1,17 @@
-/*Exercício 2*/
+// Exercício 2
 var quantDias = parseInt(prompt("Digite a quantidade de dias:"));
 var linksDia = 6;
 var curtidasLink = 8;
 var total = quantDias * linksDia * curtidasLink;
 alert("Total de curtidas recebidas em " + quantDias + "dia(s):" + total);
 
-/*Exercício 6*/
+// Exercício 6
 var horasDia = parseInt(prompt("Digite quantas horas por dia ele pode jogar:"));
 var horasMinimas = 78;
 var diasCompletos = Math.ceil(horasMinimas/horasDia);
 alert("É necessário " + diasCompletos + " dia(s) para completar o jogo");
 
-/*Exercício 1*/
+// Exercício 1
 var num1 = parseFloat(prompt("Digite o primeiro número:"));
 var num2 = parseFloat(prompt("Digite o segundo número:"));
 if (num1 > num2) {
@@ -22,7 +22,7 @@ else
     alert("O maior número é: " + num2)
 }
 
-/*Exercício 2*/
+// Exercício 2
 var valor = parseFloat(prompt("Digite um valor:"));
 if (valor > 0) {
     alert("O valor é positivo");
@@ -34,7 +34,7 @@ else {
     alert("O valor é zero");
 }
 
-/*Exercício 2*/
+// Exercício 2
 for (var i =1; i<=5; i++) {
     var num = parseInt(prompt("Digite o número:"));
     if (num % 2 == 0) {
@@ -45,7 +45,7 @@ for (var i =1; i<=5; i++) {
     }
 }
 
-/*Exercício 3*/
+// Exercício 3
 var somaPares = 0;
 var somaImpares = 0;
 for (var i = 1; i <= 100; i++) {
@@ -59,7 +59,7 @@ for (var i = 1; i <= 100; i++) {
 alert("O resultado da soma dos números pares entre 1 e 100 é:" + somaPares);
 alert("O resultado da soma dos números ímpares entre 1 e 100 é:" + somaImpares)
 
-/*Exercício 1*/
+// Exercício 1/
 function converterFahrenheit(fahrenheit) {
     var celsius = (fahrenheit - 32) * 5 / 9
     return celsius;
@@ -68,7 +68,7 @@ var temperaturaFahrenheit = parseFloat(prompt("Digite a temperatura em fahrenhei
 var temperaturaCelsius = converterFahrenheit(temperaturaFahrenheit);
 alert(temperaturaFahrenheit + "°F equivalem a: " + temperaturaCelsius.toFixed(2) + "°C");
 
-/*Exercício 3*/
+// Exercício 3
 function verificarAprovacao(nota1, nota2) {
     var notaMedia = (nota1 + nota2) / 2;
 
