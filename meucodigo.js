fa59fdc93af93b27766ce2520af72dbb45507c92
Gabriela Schubert <gabrielaschubert172@gmail.com>
2024-06-12 18:44:01 -0300

    add cod js

--- meucodigo.js
+++ meucodigo.js
@@ -1,3 +1,10 @@
+/* Exemplo de como informar algo á página */
 let nome;
 nome = prompt("Informe aqui o seu nome: ");
 alert("Olá, " + nome);
+
+/* Exemplo de como utilizar uma mensagem de confirmação para sair de uma página */
+var sair = confirm("Você deseja sair da página atual e ir para o Google?");
+if (sair == 1) {
+  window.location = "https://www.google.com/";
+}
